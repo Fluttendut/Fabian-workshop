ec2834a49316ca47e6c639c0a53b3db28570eed6
Zouthen <mich5n69@stud.kea.dk>
2023-05-11 13:17:40 +0200

    added logo to LogoPage with fading created images folder

--- js/SwitchPage.js
+++ js/SwitchPage.js
@@ -1,4 +1,4 @@
 function goToFrontpage(){
     setTimeout(function (){
-    window.location.href = '../HTML/Frontpage.html'},2000)}
+    window.location.href = '../HTML/Frontpage.html'},5000)}
 goToFrontpage()
