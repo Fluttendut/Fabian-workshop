e2277f196ade8d1004188da63e75e6983c1e2544
Zouthen <mich5n69@stud.kea.dk>
2023-05-17 10:49:24 +0200

    update

---
+++ js/product_page.js
@@ -0,0 +1,23 @@
+// const enquiryButton = document.getElementById("enquiry-button");
+// const closeButton = document.getElementById("close-button");
+// const enquiryDialog = document.getElementById("enquiry-dialog");
+// const enquiryForm = document.getElementById("enquiry-form");
+//
+// // Add an event listener to the enquiry button to open the dialog
+// enquiryButton.addEventListener("click", () => {
+//     enquiryDialog.showModal();
+// });
+//
+// // Add an event listener to the close button to close the dialog
+// closeButton.addEventListener("click", () => {
+//     enquiryDialog.close();
+// });
+//
+// // Handle form submission
+// enquiryForm.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     // Process the form data here
+//     // ...
+//     // Close the dialog after processing the form data
+//     enquiryDialog.close();
+// });
